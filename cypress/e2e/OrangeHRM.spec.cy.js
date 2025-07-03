b655e07e403913364f121b7ba1cdf11da06b28a5
ldelgado02 <luisgustavo220602@gmail.com>
2025-07-02 21:37:24 -0300

    alteração na selectors list

--- cypress/e2e/OrangeHRM.spec.cy.js
+++ cypress/e2e/OrangeHRM.spec.cy.js
@@ -1,27 +1,28 @@
 describe('Orange HRM Testes', () => {
+
+  const selectorsList = {
+    userNameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopbar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    wrongCredentialAlert: '.oxd-alert'
+  }
+
   it('Login - Sucess ', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.userNameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorsList.sectionTitleTopbar).contains('Dashboard')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('abcdefg')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert')
-  });
-
-  it('Login with username empty', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('abcdefg')
-    cy.get('.oxd-button').click()
-    cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').contains('Required')
-    
+    cy.get(selectorsList.userNameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('abcdefg1234')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   });
 
 })
